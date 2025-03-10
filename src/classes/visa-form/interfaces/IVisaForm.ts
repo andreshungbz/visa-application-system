b6f35d6d3411ad81071db06fdbc89a45db6736ef
Andres Hung <2018118240@ub.edu.bz>
2025-03-09 22:45:43 -0600

    Add setter getters for VisaForm interface

---
+++ src/classes/visa-form/interfaces/IVisaForm.ts
@@ -0,0 +1,67 @@
+// Visa Form Interface
+
+export interface IVisaForm {
+  // MAIN METHODS
+
+  // GETTERS/SETTERS
+
+  // personal section
+
+  p_getFirstName(): string;
+  p_getLastName(): string;
+  p_getEmail(): string;
+  p_getPhone(): string;
+  p_getAddress(): string;
+  p_getPassportNumber(): string;
+  p_getCountry(): string;
+  p_getDOB(): Date;
+  p_getGender(): string;
+
+  p_setFirstName(firstName: string): boolean;
+  p_setLastName(lastName: string): boolean;
+  p_setEmail(email: string): boolean;
+  p_setPhone(phone: string): boolean;
+  p_setAddress(address: string): boolean;
+  p_setPassportNumber(passportNumber: string): boolean;
+  p_setCountry(country: string): boolean;
+  p_setDOB(date: Date): boolean;
+  p_setGender(gender: string): boolean;
+
+  // travel section
+
+  t_getStayCity(): string;
+  t_getStayAddress(): string;
+  t_getIntendedArrivalDate(): Date;
+  t_getIntendedLengthOfStay(): number;
+
+  t_setStayCity(stayCity: string): boolean;
+  t_setStayAddress(stayAddress: string): boolean;
+  t_setIntendedArrivalDate(intendedIrrivalDate: Date): boolean;
+  t_setIntendedLengthOfStay(intendedLengthOfStay: number): boolean;
+
+  // work section
+
+  w_getOccupation(): string;
+  w_getPhone(): string;
+  w_getCity(): string;
+  w_getAddress(): string;
+
+  w_setOccupation(occupation: string): boolean;
+  w_setPhone(phone: string): boolean;
+  w_setCity(city: string): boolean;
+  w_setAddress(address: string): boolean;
+
+  // security section
+
+  s_getCommunicableDisease(): boolean;
+  s_getMoneyLaundering(): boolean;
+  s_getDrugConspiracy(): boolean;
+  s_getArrestedConvicted(): boolean;
+  s_getMentalPhysicalDisorder(): boolean;
+
+  s_setCommunicableDisease(checked: boolean): boolean;
+  s_setMoneyLaundering(checked: boolean): boolean;
+  s_setDrugConspiracy(checked: boolean): boolean;
+  s_setArrestedConvicted(checked: boolean): boolean;
+  s_setMentalPhysicalDisorder(checked: boolean): boolean;
+}
