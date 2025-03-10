d72a0bc8cb1859f8ffb4073ccde1267588ed4e71
Andres Hung <2018118240@ub.edu.bz>
2025-03-09 22:14:13 -0600

    Remove build files

--- build/config/app.config.js
+++
@@ -1,5 +0,0 @@
-// Filename: app.config.ts
-// basic configuration parameters for the application
-export const config = {
-    port: 3000,
-};
--- build/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-// Filename: logger.ts
-// middleware to log the time, method and URL of every HTTP request
-export const logger = (req, _res, next) => {
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
-    // https://www.localeplanet.com/icu/en-BZ/index.html
-    const now = new Date().toLocaleString('en-BZ', { hour12: true });
-    console.log(`[${now}] ${req.method} ${req.url}`);
-    next();
-};
