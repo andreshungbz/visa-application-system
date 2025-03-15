d41385d6c2dfe52fcd4d99a8419d2f9f08f29eb0
Andres Hung <2018118240@ub.edu.bz>
2025-03-15 16:42:37 -0600

    Rename log tag

--- src/utils/initialize.ts
+++ src/utils/initialize.ts
@@ -5,17 +5,17 @@ import { EmployeeSystem } from '../classes/employee-system/EmployeeSystem';
 import { VisaSystem } from '../classes/visa-system/VisaSystem';
 
 export const initializeVisaSystem = (): VisaSystem => {
-  console.time('[VAS] Visa System Initialization Loading Time');
+  console.time('[INIT] Visa System Initialization Loading Time');
   const system = new VisaSystem();
-  console.timeEnd('[VAS] Visa System Initialization Loading Time');
+  console.timeEnd('[INIT] Visa System Initialization Loading Time');
 
   return system;
 };
 
 export const initializeEmployeeSystem = (): EmployeeSystem => {
-  console.time('[VAS] Employee System Initialization Loading Time');
+  console.time('[INIT] Employee System Initialization Loading Time');
   const system = new EmployeeSystem();
-  console.timeEnd('[VAS] Employee System Initialization Loading Time');
+  console.timeEnd('[INIT] Employee System Initialization Loading Time');
 
   return system;
 };
