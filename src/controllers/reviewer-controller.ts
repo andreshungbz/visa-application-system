9aa5ade11c616cba62f02ae36198517097e47917
Andres Hung <2018118240@ub.edu.bz>
2025-04-20 16:36:44 -0600

    Create application list for b1

--- src/controllers/reviewer-controller.ts
+++ src/controllers/reviewer-controller.ts
@@ -3,6 +3,7 @@
 
 import { Request, Response } from 'express';
 import { vs } from '../main.js';
+import { VisaStatus } from '@prisma/client';
 
 // renders reviewer dashboard
 export const getReviewerDashboard = (_req: Request, res: Response) => {
@@ -16,4 +17,32 @@ export const getReviewerDashboard = (_req: Request, res: Response) => {
     finalCount,
     totalCount: initialCount + interviewCount + finalCount,
   });
+};
+
+// renders application queue
+export const getQueue = (req: Request, res: Response) => {
+  const stage = req.query.stage;
+
+  switch (stage) {
+    case 'initial':
+      const queue = vs.getInitialQueue();
+
+      queue.sort(
+        (a, b) => a.getUpdatedAt().getTime() - b.getUpdatedAt().getTime()
+      );
+
+      return res.render('reviewer/queue', {
+        stage: VisaStatus.Initial,
+        description:
+          'These are the initial applications submitted by individuals. Check for errors and inconsistencies.',
+        total: queue.length,
+        applications: queue,
+      });
+    case 'interview':
+      return res.render('reviewer/queue');
+    case 'final':
+      return res.render('reviewer/queue');
+    default:
+      return res.render('error', { message: 'Invalid Queue' });
+  }
 };
--- src/routes/reviewer.route.ts
+++ src/routes/reviewer.route.ts
@@ -2,10 +2,14 @@
 // routes for reviewer role
 
 import express from 'express';
-import { getReviewerDashboard } from '../controllers/reviewer-controller.js';
+import {
+  getQueue,
+  getReviewerDashboard,
+} from '../controllers/reviewer-controller.js';
 
 const reviewerRoute = express.Router();
 
 reviewerRoute.get('/dashboard', getReviewerDashboard);
+reviewerRoute.get('/queue', getQueue);
 
 export default reviewerRoute;
