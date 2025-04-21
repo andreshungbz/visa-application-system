2c9de251b92a07d0a91209cb25474b74f2e730e9
Andres Hung <2018118240@ub.edu.bz>
2025-04-21 12:50:25 -0600

    Render application lists

--- src/controllers/supervisor-controller.ts
+++ src/controllers/supervisor-controller.ts
@@ -3,7 +3,47 @@
 
 import { Request, Response } from 'express';
 
+import { vs } from '../main.js';
+import { VisaStatus } from '@prisma/client';
+
 // renders supervisor dashboard
 export const getSupervisorDashboard = (_req: Request, res: Response) => {
   res.render('supervisor/dashboard');
+};
+
+// renders application list
+export const getList = (req: Request, res: Response) => {
+  const status = req.query.status;
+
+  switch (status) {
+    case 'approved':
+      const approvedApplications = vs.getApproved();
+
+      approvedApplications.sort(
+        (a, b) => a.getUpdatedAt().getTime() - b.getUpdatedAt().getTime()
+      );
+
+      return res.render('supervisor/list', {
+        stage: VisaStatus.Approved,
+        description:
+          'These are the applications that have passed all three stages',
+        total: approvedApplications.length,
+        applications: approvedApplications,
+      });
+    case 'rejected':
+      const rejectedApplications = vs.getRejected();
+
+      rejectedApplications.sort(
+        (a, b) => a.getUpdatedAt().getTime() - b.getUpdatedAt().getTime()
+      );
+
+      return res.render('supervisor/list', {
+        stage: VisaStatus.Rejected,
+        description: 'These applications have been rejected at any stage.',
+        total: rejectedApplications.length,
+        applications: rejectedApplications,
+      });
+    default:
+      return res.render('error', { message: 'Invalid List' });
+  }
 };
--- src/routes/supervisor-route.ts
+++ src/routes/supervisor-route.ts
@@ -2,10 +2,14 @@
 // routes for system supervisor role
 
 import express from 'express';
-import { getSupervisorDashboard } from '../controllers/supervisor-controller.js';
+import {
+  getList,
+  getSupervisorDashboard,
+} from '../controllers/supervisor-controller.js';
 
 const supervisorRoute = express.Router();
 
 supervisorRoute.get('/dashboard', getSupervisorDashboard);
+supervisorRoute.get('/list', getList);
 
 export default supervisorRoute;
