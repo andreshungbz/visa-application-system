19c601e235bca7ebab08e80418d579fa7c7f9d23
Andres Hung <2018118240@ub.edu.bz>
2025-04-21 13:04:32 -0600

    Implement viewing approved/rejected applications

--- src/controllers/supervisor-controller.ts
+++ src/controllers/supervisor-controller.ts
@@ -4,7 +4,7 @@
 import { Request, Response } from 'express';
 
 import { vs } from '../main.js';
-import { VisaStatus } from '@prisma/client';
+import { VisaStatus, VisaType } from '@prisma/client';
 
 // renders supervisor dashboard
 export const getSupervisorDashboard = (_req: Request, res: Response) => {
@@ -46,4 +46,53 @@ export const getList = (req: Request, res: Response) => {
     default:
       return res.render('error', { message: 'Invalid List' });
   }
+};
+
+// renders application view page
+export const getView = (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    return res.render('error', { message: 'Invalid Application Number' });
+  }
+
+  const application = vs.getFullVisaApplication(id);
+
+  if (!application) {
+    return res.render('error', {
+      message: `Application with ID ${id} Not Found`,
+    });
+  }
+
+  let prevStatus: string;
+  switch (application.getStatus()) {
+    case VisaStatus.Approved:
+      prevStatus = 'approved';
+      break;
+    case VisaStatus.Rejected:
+      prevStatus = 'rejected';
+      break;
+    default:
+      return res.render('error', { message: 'Invalid Visa Status' });
+  }
+
+  console.log(application.getS1Reviewer());
+
+  res.render('supervisor/view', {
+    application,
+    prev: prevStatus,
+    isB1: application.getType() === VisaType.B1,
+    isB2: application.getType() === VisaType.B2,
+    isF1: application.getType() === VisaType.F1,
+    isInterview: false,
+    isFinal: false,
+    isFinished: true,
+    s1Reviewer: application.getS1Reviewer(),
+    s1Notes: application.getS1Notes(),
+    s2Reviewer: application.getS2Reviewer(),
+    s2Notes: application.getS2Notes(),
+    s3Reviewer: application.getS3Reviewer(),
+    s3Notes: application.getS3Notes(),
+    editable: false,
+  });
 };
--- src/routes/supervisor-route.ts
+++ src/routes/supervisor-route.ts
@@ -5,11 +5,13 @@ import express from 'express';
 import {
   getList,
   getSupervisorDashboard,
+  getView,
 } from '../controllers/supervisor-controller.js';
 
 const supervisorRoute = express.Router();
 
 supervisorRoute.get('/dashboard', getSupervisorDashboard);
 supervisorRoute.get('/list', getList);
+supervisorRoute.get('/view/:id', getView);
 
 export default supervisorRoute;
