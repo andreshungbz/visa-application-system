831053a889e930f987d92277d7fe07d9b0f80a90
Andres Hung <2018118240@ub.edu.bz>
2025-04-21 16:13:15 -0600

    Generate

--- src/db/data.ts
+++ src/db/data.ts
@@ -1,12 +1,13 @@
 // Filename: data.ts
 // inserts example data to work with
 
-import * as Prisma from '@prisma/client';
-
-import { PrismaClient } from '@prisma/client';
+import {
+  EmployeeType,
+  PrismaClient,
+  VisaStatus,
+  VisaType,
+} from '@prisma/client';
 import argon2 from 'argon2';
-
-const { EmployeeType, VisaStatus, VisaType } = Prisma;
 
 const prisma = new PrismaClient();
 
